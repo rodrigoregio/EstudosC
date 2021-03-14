04e0ea291674374619d473fc4dd79d2685012dd7
rodrigo regio <rodrigo_1567@hotmail.com>
2021-03-14 08:28:18 -0300

    Resolução de alguns algoritmos dos videos do professor Douglas Maioli

--- vetores.cpp
+++ vetores.cpp
@@ -1,35 +1,34 @@
 #include <iostream>
-using namespace std;
 float teste_estatico(){
     int notas[4];
     int soma=0;
-    cout << "Informe as notas a partir da semana 3 até a semana 6" << endl;
+    std::cout << "Informe as notas a partir da semana 3 até a semana 6" << std::endl;
     for(int i=0;i<4;i++){
-        cout << "Informe a nota da semana " << (i+3) << endl;
-        cin >> notas[i];
+        std::cout << "Informe a nota da semana " << (i+3) << std::endl;
+        std::cin >> notas[i];
         soma += notas[i];
     }
     int media=soma/4;
-    cout << "A media das notas foi: " << media << "\nA soma das medias foi: " << soma;
+    std::cout << "A media das notas foi: " << media << "\nA soma das medias foi: " << soma;
     return media;
 }
 int teste_dinamico(){
     int* notas=new int[4];
     int soma=0;
-    cout << "Informe as notas a partir da semana 3 até a semana 6" << endl;
+    std::cout << "Informe as notas a partir da semana 3 até a semana 6" << std::endl;
     for(int i=0;i<4;i++){
-        cout << "Informe a nota da semana " << (i+3) << endl;
-        cin >> notas[i];
+        std::cout << "Informe a nota da semana " << (i+3) << std::endl;
+        std::cin >> notas[i];
         soma += notas[i];
     }
-    cout << "A media das notas foi: " << (soma/4) << endl;
-    cout << "A soma das medias foi: " << soma << endl;
+    std::cout << "A media das notas foi: " << (soma/4) << std::endl;
+    std::cout << "A soma das medias foi: " << soma << std::endl;
     delete [] notas;
     return (soma/4);
 }
 int main(){
-    cout << "Embora que de forma diferente, ambos devem dar o mesmo resultado.\n(claro se forem imputados os mesmos valores :)" << endl;
-    cout << teste_estatico() << endl;
-    cout << teste_dinamico() << endl;
+    std::cout << "Embora que de forma diferente, ambos devem dar o mesmo resultado.\n(claro se forem imputados os mesmos valores :)" << std::endl;
+    std::cout << teste_estatico() << std::endl;
+    std::cout << teste_dinamico() << std::endl;
     return 0;
 }
